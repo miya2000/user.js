9f89a1358fe879e73012c7461cce411c773974f4
unknown <miya2000.opera@gmail.com>
2009-06-14 17:58:32 +0900

    support hot.gyao.jp, utauga.jp

--- gyao_in_opera.js
+++ gyao_in_opera.js
@@ -3,14 +3,17 @@
 // @description Open GyaO movie directly.
 // @version 1.1.0
 // @include http://www.gyao.jp/*
+// @include http://hot.gyao.jp/*
+// @include http://utauga.jp/karaoke/*
 // @exclude http://*http://*
+// @exclude http://*http%3A%2F%2F*
 // ==/UserScript==
 (function() {
 
-    // alert off
+    // ignore "event" script
     window.opera.addEventListener('BeforeScript', function (e) {
         var ev = e.element.getAttribute('event');
-        if ( ev && ev.toLowerCase() == 'click()' ) {
+        if (ev) {
             e.preventDefault();
         }
     }, false );
@@ -18,11 +21,14 @@
     // overwrite browser determine.
     window.opera.defineMagicVariable('canPlay', function() { return true }, null);
 
-    // remove 'showRecommend'.
+    // do nothing on 'showRecommend'.
     window.opera.defineMagicFunction('showRecommend', function(){});
 
-    // open asx directly.
-    if (/sityou/.test(location.href)) {
+    // avoid cloaking.
+    document.all = true;
+
+    //=== www.gyao.jp ===
+    if (/http:\/\/www\.gyao\.jp\/sityou\/movie\//.test(location.href)) {
         window.opera.defineMagicFunction('createWmpObject',
             function(orgFunc, orgThis, ID, WIDTH, HEIGHT, URL, UIMODE, STRETCHTOFIT, PLAYCOUNT, VOLUME) {
                 if (window.opener) {
@@ -39,5 +45,41 @@
             }
         );
     }
-})()
 
+    //=== hot.gyao.jp ===
+    if (/http:\/\/hot\.gyao\.jp\/sityou\/catedetail\/contents_id\//.test(location.href)) {
+        window.opera.defineMagicFunction('createWmpObject',
+            function(orgFunc, orgThis, ID, WIDTH, HEIGHT, URL, UIMODE, STRETCHTOFIT, PLAYCOUNT, VOLUME) {
+                var iframe = document.createElement('iframe');
+                iframe.src = URL;
+                iframe.style.display = 'none';
+                document.body.appendChild(iframe);
+            }
+        );
+    }
+
+    //=== utauga.jp ===
+    if (/^http:\/\/utauga\.jp\/karaoke\//.test(location.href)) {
+        window.opera.defineMagicFunction('changeMedia',
+            function(orgFunc, orgThis, id, viewRank) {
+                var iframe = document.getElementById('wmviFrame');
+                var URL='/karaoke/asx.php?id='+id+'&log=0';
+                iframe.src = URL;
+                if (viewRank) {
+                    var rank=document.getElementById('rankiFrame');
+                    rank.src="/karaoke/play/play.php?id="+id;
+                }
+            }
+        );
+    }
+    if (/^http:\/\/utauga\.jp\/karaoke\/(index\.php)?\?/.test(location.href)) {
+        var m;
+        if (m = /contents_id=([^&]+)/.exec(location.search)) {
+            var contents_id = m[1];
+            document.addEventListener('load', function() { changeMedia(contents_id); }, false);
+        }
+    }
+    if (/^http:\/\/utauga\.jp\/karaoke\/wmv\.php/.test(location.href)) {
+        window.opera.defineMagicFunction('createWmpObject', function(){});
+    }
+})()
